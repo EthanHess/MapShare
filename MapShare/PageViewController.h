29c1848d0a3bca972007b9b0f5f706477ec5201b
Ethan Hess <ech1988@gmail.com>
2015-04-15 17:33:26 -0600

    table view appearance work
    Signed-off-by: Ethan Hess <ech1988@gmail.com>

---
+++ MapShare/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  MapShare
+//
+//  Created by Ethan Hess on 4/15/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController
+
+@end
---
+++ MapShare/PageViewController.m
@@ -0,0 +1,37 @@
+//
+//  PageViewController.m
+//  MapShare
+//
+//  Created by Ethan Hess on 4/15/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import "PageViewController.h"
+
+@interface PageViewController ()
+
+@end
+
+@implementation PageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
